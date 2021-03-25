9222f8479a8a889daacd8546e490992965068913
Nicole <nicoleshek315@gmail.com>
2021-03-25 16:05:24 +0200

    test

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,9 +1,4 @@
 import { SignIn } from '@clerk/clerk-react';
 export default function SignInPage() {
-    return (
-        <div>
-            <h1 style="text-align:center;">Hi this is Nicole!</h1>
-            <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />
-        </div>
-    );
+    return <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />;
 }
--- pages/sign-up/[[...index]].js
+++ pages/sign-up/[[...index]].js
@@ -1,8 +1,4 @@
 import { SignUp } from '@clerk/clerk-react';
 export default function SignUpPage() {
-    return (
-        <div>
-            <SignUp path="/sign-up" routing="path" signInURL="/sign-in" />
-        </div>
-    );
+    return <SignUp path="/sign-up" routing="path" signInURL="/sign-in" />;
 }
