6cde7c24e4c9844ff8149e389f68569710f55c21
Nicole <nicoleshek315@gmail.com>
2021-03-25 16:00:12 +0200

    fixed bugs

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,8 +1,9 @@
 import { SignIn } from '@clerk/clerk-react';
 export default function SignInPage() {
-    return;
-    <div>
-        <h1 style="text-align:center;">Hi this is Nicole!</h1>
-        <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />
-    </div>;
+    return (
+        <div>
+            <h1 style="text-align:center;">Hi this is Nicole!</h1>
+            <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />
+        </div>
+    );
 }
--- pages/sign-up/[[...index]].js
+++ pages/sign-up/[[...index]].js
@@ -1,8 +1,8 @@
 import { SignUp } from '@clerk/clerk-react';
 export default function SignUpPage() {
-    return;
-    <h1>
-        Hello world!
-        <SignUp path="/sign-up" routing="path" signInURL="/sign-in" />
-    </h1>;
+    return (
+        <div>
+            <SignUp path="/sign-up" routing="path" signInURL="/sign-in" />
+        </div>
+    );
 }
