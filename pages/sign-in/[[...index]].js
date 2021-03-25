bcff1acb0b95d8ff58c6a9d333fdae9a29d115fe
Nicole <nicoleshek315@gmail.com>
2021-03-25 21:24:14 +0200

    add h1

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -1,4 +1,9 @@
 import { SignIn } from '@clerk/clerk-react';
 export default function SignInPage() {
-    return <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />;
+    return (
+        <div>
+            <h1 style={{textAlign:"center"}}>Hello!</h1>
+            <SignIn path="/sign-in" routing="path" signUpURL="/sign-up" />
+        </div>
+    );
 }
